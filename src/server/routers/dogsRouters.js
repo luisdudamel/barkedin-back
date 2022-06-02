cdadfd41942e182e15407ab1cf11001405e7042a
Luis Dudamel <ladusa@gmail.com>
2022-06-02 21:35:36 +0200

    Added auth middleware to favdogs

--- src/server/routers/dogsRouters.js
+++ src/server/routers/dogsRouters.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const { getFavDogs } = require("../controllers/dogControllers");
+const { auth } = require("../middlewares/auth");
 
 const dogsRouter = express.Router();
 
-dogsRouter.get("/favdogs", getFavDogs);
+dogsRouter.get("/favdogs", auth, getFavDogs);
 
 module.exports = dogsRouter;
