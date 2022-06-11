01925a705a87a7d96a48fd3324b8a3e80cec6296
Luis Dudamel <ladusa@gmail.com>
2022-06-11 11:06:32 +0200

    Fixed incoming object for editing

--- src/server/controllers/dogControllers.js
+++ src/server/controllers/dogControllers.js
@@ -90,10 +90,10 @@ const createFavDog = async (req, res, next) => {
 
 const editFavDog = async (req, res, next) => {
   try {
-    const { updatedDog } = req.body;
+    const { newDog } = req.body;
 
     const { file } = req;
-    const updatedDogParsed = JSON.parse(updatedDog);
+    const updatedDogParsed = JSON.parse(newDog);
     if (file) {
       const newFileName = `${Date.now()}-${file.originalname}`;
       fs.rename(
