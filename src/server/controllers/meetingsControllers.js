6ff9ee0b31563620358c20849a167a581ad27ef0
Luis Dudamel <ladusa@gmail.com>
2022-08-16 17:54:46 +0200

    Added meetings controller

--- src/server/controllers/meetingsControllers.js
+++ src/server/controllers/meetingsControllers.js
@@ -1,5 +1,11 @@
-const getAllMeetings = (req, res, next) => {
-  res.status(200).json({ message: "Llego aqui la request" });
+const debug = require("debug")("barkedin:server:controller:meetings");
+const chalk = require("chalk");
+const Meeting = require("../../database/models/Meeting");
+
+const getAllMeetings = async (req, res, next) => {
+  const meetings = await Meeting.find();
+  res.status(200).json({ meetings });
+  debug(chalk.yellow("A request to get all meetings have been made"));
 };
 
 module.exports = getAllMeetings;
