a33dbae2b22b1585a2aae2262b6bc51594a3f823
Luis Dudamel <ladusa@gmail.com>
2022-08-17 10:37:32 +0200

    Added populate and auth to allmeetings response

--- src/server/controllers/meetingsControllers.js
+++ src/server/controllers/meetingsControllers.js
@@ -1,11 +1,20 @@
 const debug = require("debug")("barkedin:server:controller:meetings");
 const chalk = require("chalk");
 const Meeting = require("../../database/models/Meeting");
+const Dog = require("../../database/models/Dog");
 
 const getAllMeetings = async (req, res, next) => {
-  const meetings = await Meeting.find();
-  res.status(200).json({ meetings });
-  debug(chalk.yellow("A request to get all meetings have been made"));
+  try {
+    const meetings = await Meeting.find().populate({
+      path: "dog",
+      Dog,
+    });
+    res.status(200).json({ meetings });
+    debug(chalk.yellow("A request to get all meetings have been made"));
+  } catch (error) {
+    error.customMessage = "Error querying all meetings";
+    next(error);
+  }
 };
 
 module.exports = getAllMeetings;
--- src/server/routers/meetingsRouters.js
+++ src/server/routers/meetingsRouters.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const getAllMeetings = require("../controllers/meetingsControllers");
+const { auth } = require("../middlewares/auth");
 
 const meetingsRouter = express.Router();
 
-meetingsRouter.get("/", getAllMeetings);
+meetingsRouter.get("/", auth, getAllMeetings);
 
 module.exports = meetingsRouter;
