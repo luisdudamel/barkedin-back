b4ee35595eb4e3da64ce0162975eaf018c2071a2
Luis Dudamel <ladusa@gmail.com>
2022-11-12 20:44:11 +0100

    Added error handling for resizeimage middleware

--- src/server/middlewares/fileResize.js
+++ src/server/middlewares/fileResize.js
@@ -3,15 +3,22 @@ const { readFile, writeFile } = require("fs/promises");
 
 const resizeImage = async (req, res, next) => {
   const fileName = req.file.filename;
-  const fileToResize = await readFile(`uploads/images/${fileName}`);
 
-  const imageResized = await sharp(fileToResize)
-    .webp({ quality: 70 })
-    .toBuffer();
+  try {
+    const fileToResize = await readFile(`uploads/images/${fileName}`);
 
-  await writeFile(`uploads/images/${fileName}`, imageResized);
+    const imageResized = await sharp(fileToResize)
+      .webp({ quality: 70 })
+      .toBuffer();
 
-  next();
+    await writeFile(`uploads/images/${fileName}`, imageResized);
+
+    next();
+  } catch (error) {
+    error.customMessage = "Error resizing image";
+    error.statusCode = 500;
+    next(error);
+  }
 };
 
 module.exports = resizeImage;
